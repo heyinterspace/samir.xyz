569b7623e715a844e7b91a36d3150b73b25f1491
Samir <hey@samir.xyz>
2025-01-05 16:04:25 +0000

    Automatic commit before Assistant edits

--- client/js/main.js
+++
@@ -1,9 +0,0 @@
-const container = document.getElementById('root');
-if (!container) throw new Error('Failed to find the root element');
-
-const root = ReactDOM.createRoot(container);
-root.render(
-  React.createElement(React.StrictMode, null,
-    React.createElement(App)
-  )
-);
