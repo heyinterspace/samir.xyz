3a2624263291626a434ec6a66b160517463b1e95
heysamir <32209712-heysamir@users.noreply.replit.com>
2025-01-05 13:50:29 +0000

    Agent query: Is the React application now running with the navigation bar visible?
    Remove react-query and toaster components
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/d2193f08-b592-45ce-b730-8dc2c7ef133c/d8a789a5-829c-401e-9b7e-2f346e4c7848.jpg

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/queryClient";
-import { Toaster } from "./components/ui/toaster";
 import App from './App';
 import "./index.css";
 
@@ -11,9 +8,6 @@ if (!rootElement) throw new Error('Failed to find the root element');
 
 createRoot(rootElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <Toaster />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
 );
