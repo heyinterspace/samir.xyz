1066019ca13611fb824bb131a986af4c7f5ceae5
heysamir <32209712-heysamir@users.noreply.replit.com>
2025-02-27 09:37:56 +0000

    New agent session start

--- config/styles/postcss.config.js
+++ config/styles/postcss.config.js
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    tailwindcss: { config: './config/tailwind.config.js' },
+    tailwindcss: { config: './config/styles/tailwind.config.js' },
     autoprefixer: {},
   },
 }
